d4117c7a170b1a601a5abb4d1e27781d313ffc83
Nikita Nacharov <nikita2220034@gmail.com>
2023-01-16 17:00:59 +0300

    added web.xml  & servlet

--- Server/src/ru/studwork/project/server/servlet.java
+++ Server/src/ru/studwork/project/server/servlet.java
@@ -1,11 +1,30 @@
-//package ru.studwork.project.server;
+package ru.studwork.project.server;
 
-//public class servlet extends HttpServlet {
-   // @Override
-   // protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-  //      super.doGet(req,resp);
-     //   PrintWriter writer = resp.getWriter();
-        //writer.println("PEPEGA!");
-       // writer.close();
-  //  }
-//}
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Enumeration<String> parameterNames = req.getParameterNames();
+        resp.setContentType("text/html");
+        String html = "<html><body><h1>Parameters:<br>";
+        while (parameterNames.hasMoreElements()){
+            String name = parameterNames.nextElement();
+            String value = req.getParameter(name);
+            html+=name+" = "+value+"<br>";
+        }
+        html+="</h1></body></html>";
+
+        PrintWriter writer = resp.getWriter();
+        writer.println(html);
+        writer.close();
+    }
+}
